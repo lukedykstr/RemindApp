2fe9b8f3758964caf4c3eb720f946fd06201e3dd
Luke Dykstra <122311615+lukedykstr@users.noreply.github.com>
2023-03-02 21:32:22 +0000

    edit functionality, implement settings menu, fix bugs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // today's date. will stay the same
-var TodaysDate = new Date().toLocaleDateString('en-US');
+var TodaysDate = new Date();
 // the date that's currently being viewed. starts as today's date. will change
 var DisplayDate = TodaysDate;
 
@@ -13,83 +13,121 @@ var settings = {
 // [{header, body, date, time}, {...}, ...]
 var reminders = [];
 
+var headerInput = document.getElementById("HEADER");
+var bodyInput = document.getElementById("bodyText");
+var dateInput = document.getElementById("DATE");
+var timeInput = document.getElementById("TIME");
+
+var emailInput = document.getElementById("email");
+var phoneInput = document.getElementById("phone");
+
 //trying to figure out how to create html objects using javascript
 function createNode() {
   var breakLine = document.createElement("br");
-
   var doc = document.createElement("div");
-  doc.setAttribute('class', 'RemindBubble');
-
   var text = document.createElement("h1");
+  var bodyText = document.createElement("p");
+  var dateFormat = document.createElement("time");
+  var date = new Date(document.getElementById("DATE").value);
+  var timeFormat = document.createElement("time");
+  doc.setAttribute("class", "RemindBubble");
+
+
   text.classList.add("RemindH");
   text.innerHTML = document.getElementById("HEADER").value;
   doc.appendChild(text);
 
-  var bodyText = document.createElement("p");
   bodyText.classList.add("RemindP");
   bodyText.innerHTML = document.getElementById("bodyText").value;
   doc.appendChild(bodyText);
 
-  var dateFormat = document.createElement("time");
-  
+
   // date value from date input is in YYYY-MM-DD form and is one day behind
   // for some reason, idk why
   // this changes it to MM/DD/YYYY and increments it by 1
-  var date = new Date(document.getElementById("DATE").value);
   date.setDate(date.getDate() + 1);
-  
-  dateFormat.innerHTML = date.toLocaleDateString('en-US');
+  dateFormat.innerHTML = date.toLocaleDateString("en-US");
   doc.appendChild(dateFormat);
 
   doc.appendChild(breakLine);
-
-  var timeFormat = document.createElement("time");
   timeFormat.innerHTML = document.getElementById("TIME").value;
   doc.appendChild(timeFormat);
+  document.getElementById("Canvas").append(doc);
 
-  document.getElementById("Canvas").append(doc)
-  
   // add reminder to list
   createReminder(text.innerHTML, bodyText.innerHTML, dateFormat.innerHTML, timeFormat.innerHTML);
   save();
   //window.setTimeout(CloneNode, 0);
 }
 
-function refreshScreen() {
-  let get = Array.from(document.getElementsByClassName('RemindBubble'));
-  
-  get.forEach(element => {
-    element.remove();
-  });
-  
-  document.getElementById("DailyView").innerHTML = "Daily View: " + DisplayDate;
-  document.getElementById("DisDate").value = new Date(DisplayDate).toISOString().split('T')[0];
-  loadReminders(DisplayDate);
-}
+function formEnter() {
+  if(headerInput.value == "") {
+    alert("Header cannot be blank.");
+    return 0;
+  }
 
-function incrementDate(dateInput, increment) {
-  var dateFormatTotime = new Date(dateInput);
-  var increasedDate = new Date(dateFormatTotime.getTime() + (increment * 86400000));
-  return increasedDate;
-}
+  if(dateInput.value == "") {
+    alert("Date cannot be blank.");
+    return 0;
+  }
 
-function changeDate(amount) {
-  DisplayDate = incrementDate(DisplayDate, amount).toLocaleDateString('en-US');
+  if(timeInput.value == "") {
+    alert("Time cannot be blank.");
+    return 0;
+  }
+
+  TodaysDate = new Date();
+
+  if(new Date(dateInput.value).getTime() + 86400000 < TodaysDate.getTime()) {
+    alert("Date cannot be in the past.");
+    return 0;
+  }
+  
+  createNode();
+  closeForm();
   refreshScreen();
 }
 
-function calSubmit(){
-  DisplayDate = new Date(incrementDate(document.getElementById("DisDate").value, 1)).toLocaleDateString('en-US');
+function dateString(date) {
+  return date.toLocaleString("en-US").split(",")[0];
+}
+
+function refreshScreen() {
+  var date = dateString(DisplayDate);
+  var get = Array.from(document.getElementsByClassName("RemindBubble"));
+
+  get.forEach(function(element) {
+    element.remove();
+  });
+
+  document.getElementById("DailyView").innerHTML = "Daily View: " + date;
+  document.getElementById("DisDate").valueAsDate = DisplayDate;
+  loadReminders(date);
+}
+
+function incrementDate(dateInput, increment) {
+  return new Date(dateInput.getTime() + (86400000 * increment));
+}
+
+function changeDate(amount) {
+  DisplayDate =
+    incrementDate(DisplayDate, amount);
+  refreshScreen();
+}
+
+function calSubmit() {
+  DisplayDate = new Date(document.getElementById("DisDate").value + "T00:00:00");
   refreshScreen();
 }
 
 // shows the form. set arg to 'reminder' to show create reminder form
-// use 'settings' to show settings
+// use 'edit' to show edit reminder form
 function openForm(mode) {
-  if(mode == "reminder") {
+  if (mode === "reminder") {
+    document.getElementById("enter").setAttribute("onclick", "formEnter()");
     document.getElementById("form-title").innerHTML = "New Reminder";
-  } else if(mode == "settings") {
-    document.getElementById("form-title").innerHTML = "Settings";
+  } else if (mode === "edit") {
+    document.getElementById("form-title").innerHTML = "Edit Reminder";
   }
 
   document.getElementById("myForm").style.display = "block";
@@ -101,22 +139,47 @@ function closeForm() {
   document.getElementById("BackgroundDim").style.display = "none";
 }
 
+function openSettings() {
+  emailInput.value = settings.email;
+  phoneInput.value = settings.phone;
+  
+  document.getElementById("settingsForm").style.display = "block";
+  document.getElementById("BackgroundDim").style.display = "block";
+}
+
+function closeSettings() {
+  document.getElementById("settingsForm").style.display = "none";
+  document.getElementById("BackgroundDim").style.display = "none";
+}
+
+function settingsEnter() {
+  settings.email = emailInput.value;
+  settings.phone = phoneInput.value;
+  
+  save();
+  closeSettings();
+}
+
 //closeForm();
 
 function createReminder(header, body, date, time) {
+  reminders.push({ body, date, header, time });
+  /*
   reminders.push({
     header: header,
     body: body,
     date: date,
     time: time
   });
+  */
 }
 
 // load reminders from list given a specific date
 // use "all" to just load all
 function loadReminders(date) {
-  for (var i = 0; i < reminders.length; i++) {
-    if (date == "all" || reminders[i].date == date) {
+  var i = 0;
+  for (i = 0; i < reminders.length; i += 1) {
+    if (date === "all" || reminders[i].date === date) {
       loadNode(i);
     }
   }
@@ -126,41 +189,41 @@ function loadReminders(date) {
 function loadNode(index) {
   var data = reminders[index];
   var breakLine = document.createElement("br");
-
   var doc = document.createElement("div");
+  var text = document.createElement("h1");
+  var bodyText = document.createElement("p");
+  var dateFormat = document.createElement("time");
+  var timeFormat = document.createElement("time");
+  var buttonDiv = document.createElement("div");
+  var editButton = document.createElement("button");
+  var deleteButton = document.createElement("button");
   doc.classList.add("RemindBubble");
   //doc.setAttribute('id', 'remindBubbles');
 
-  var text = document.createElement("h1");
+
   text.classList.add("RemindH");
   text.innerHTML = data.header;
   doc.appendChild(text);
 
-  var bodyText = document.createElement("p");
   bodyText.classList.add("RemindP");
   bodyText.innerHTML = data.body;
   doc.appendChild(bodyText);
 
-  var dateFormat = document.createElement("time");
   dateFormat.innerHTML = data.date;
   doc.appendChild(dateFormat);
 
   doc.appendChild(breakLine);
 
-  var timeFormat = document.createElement("time");
   timeFormat.innerHTML = data.time;
   doc.appendChild(timeFormat);
 
   doc.appendChild(document.createElement("br"));
 
-  var buttonDiv = document.createElement("div");
   buttonDiv.style = "display:inline-block;";
 
-  var editButton = document.createElement("button");
   editButton.innerHTML = "Edit";
   editButton.setAttribute("onclick", "editReminder(" + index + ")");
 
-  var deleteButton = document.createElement("button");
   deleteButton.innerHTML = "Delete";
   deleteButton.setAttribute("onclick", "deleteReminder(" + index + ")");
 
@@ -169,12 +232,18 @@ function loadNode(index) {
 
   doc.appendChild(buttonDiv);
 
-  document.getElementById("Canvas").appendChild(doc);
+  document.getElementById("RemindContainer").appendChild(doc);
 }
 
-// work in progress. will open form to edit a certain reminder
+// opens form to edit reminder information at a certain index in reminders list
 function editReminder(index) {
-  
+  headerInput.value = reminders[index].header;
+  bodyInput.value = reminders[index].body;
+  dateInput.valueAsDate = new Date(reminders[index].date);
+  timeInput.value = reminders[index].time;
+
+  document.getElementById("enter").setAttribute("onclick", "deleteReminder(" + index + "); formEnter();");
+  openForm("edit");
 }
 
 // pops item from reminders list, saves, refreshes
@@ -186,20 +255,20 @@ function deleteReminder(index) {
 
 // save data from object to JSON file
 function save() {
-  var remindJSON  = JSON.stringify(reminders);
+  var remindJSON = JSON.stringify(reminders);
   var settingJSON = JSON.stringify(settings);
-  
+
   localStorage.setItem("reminders", remindJSON);
   localStorage.setItem("settings", settingJSON);
 }
 
-// load data from file input element
+// load data from localStorage
 function load() {
   reminders = JSON.parse(localStorage.getItem("reminders"));
-  settings  = JSON.parse(localStorage.getItem("settings"));
+  settings = JSON.parse(localStorage.getItem("settings"));
 
   // if no file exists
-  if(reminders == null) {
+  if (reminders === null) {
     // set to default values
     settings = {
       email: "",
@@ -211,11 +280,12 @@ function load() {
 }
 
 window.onload = function init() {
-  document.getElementById("DailyView").innerHTML = "Daily View: " + TodaysDate;
-  document.getElementById("DisDate").value = new Date(TodaysDate).toISOString().split('T')[0];
-
-  document.getElementById("TodaysDate").innerHTML = TodaysDate;
+  var date = dateString(TodaysDate);
+  
+  document.getElementById("DailyView").innerHTML = "Daily View: " + date;
+  document.getElementById("DisDate").valueAsDate = TodaysDate;
+  document.getElementById("TodaysDate").innerHTML = date;
 
   load();
   refreshScreen();
-}
+};
